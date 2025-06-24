a918e06b6bb0b6b50fd8ea2be9e315ffef432c5a
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-24 10:45:07 +0545

    Add socket binding and client connection acceptance

--- server.c
+++ server.c
@@ -15,6 +15,23 @@ int main() {
     server_addr.sin_addr.s_addr = INADDR_ANY; // bind to all available network interfaces (example: 127.0.0.1)
     
     // bind the socket to port
+    int bind_resposne = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    
+    // listen for the connection
+    listen(server_fd, 2);
+    
+    printf("Server is running on port 8080");
+    
+    // accept client connection
+    int client_fd;
+    
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    
+    accept(client_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    
+    
+    
     
     return 0;
 }
