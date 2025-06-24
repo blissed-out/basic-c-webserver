5e8c8f8e28043f6bab502f5f240f6bf747499617
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-24 20:38:24 +0545

    Add content type detection for HTTP responses

--- server.c
+++ server.c
@@ -5,6 +5,24 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+const char* get_content_type(const char *filename) {
+    // find the last '.' in the filename
+    const char *ext = strrchr(filename, '.');
+    if (!ext) return "application/octet-stream";  // default binary
+
+    if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0) return "text/html";
+    if (strcmp(ext, ".css") == 0) return "text/css";
+    if (strcmp(ext, ".js") == 0) return "application/javascript";
+    if (strcmp(ext, ".json") == 0) return "application/json";
+    if (strcmp(ext, ".png") == 0) return "image/png";
+    if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) return "image/jpeg";
+    if (strcmp(ext, ".gif") == 0) return "image/gif";
+    if (strcmp(ext, ".svg") == 0) return "image/svg+xml";
+    if (strcmp(ext, ".txt") == 0) return "text/plain";
+
+    return "application/octet-stream";
+}
+
 int main() {
   printf("main function called\n");
   int server_fd;
@@ -46,22 +64,30 @@ int main() {
     buffer[recv_response] = '\0';
 
     char method[10], path[999];
-    sscanf(buffer, "%s %s", method, path);
+    sscanf(buffer, "%s %s", method, path); // GET /index.html
 
-    FILE *file = fopen(path + 1, "r");
+    char full_path[1024];
+    if (strcmp(path, "/") == 0) {
+        strcpy(full_path, "index.html");
+    } else {
+        snprintf(full_path, sizeof(full_path), "%s", path + 1); // index.html
+    }
 
+    FILE *file = fopen(full_path, "r");
+
+    // send 404 response if file not found
     if (file == NULL) {
       perror("file open failed");
       char failure_message[] = "<html><body><h1>404 Not Found</h1></body></html>";
       char buffer[999];
-      
+
       snprintf(buffer, sizeof(buffer),
                "HTTP/1.1 404 Not Found\r\n"
                "Content-Length: %lu\r\n"
                "Content-Type: text/html\r\n"
                "\r\n"
                , strlen(failure_message));
-      
+
       send(client_fd, buffer, strlen(buffer), 0);
       send(client_fd, failure_message, strlen(failure_message), 0);
       close(client_fd);
@@ -77,12 +103,17 @@ int main() {
     fclose(file);
 
     char header[999];
+    const char *content_type = get_content_type(full_path);
+
     snprintf(header, sizeof(header),
              "HTTP/1.1 200 OK\r\n"
              "Content-Length: %ld\r\n"
-             "Content-Type: text/html\r\n"
+             "Content-Type: %s\r\n"
              "\r\n",
-             filesize);
+             filesize,
+             content_type);
+    
+    printf("this is header: %s", header);
 
     send(client_fd, header, strlen(header), 0);
     send(client_fd, body, strlen(body), 0);
