84b505ed0acd3beaa283474d79db00e6035c6819
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-24 22:57:08 +0545

    Extract client handling into separate function

--- server.c
+++ server.c
@@ -25,6 +25,84 @@ const char* get_content_type(const char *filename) {
     return "application/octet-stream";
 }
 
+void handle_client(int server_fd) {
+    while (1) {
+
+      int client_fd;
+
+      struct sockaddr_in client_addr;
+      socklen_t client_addr_len = sizeof(client_addr);
+
+      client_fd =
+          accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+      // recieve data from client
+      char buffer[7 * 1024 * 1024]; // 7 mb
+      ssize_t recv_response = recv(client_fd, buffer, sizeof(buffer), 0);
+
+      buffer[recv_response] = '\0';
+
+      char method[10], path[999];
+      sscanf(buffer, "%s %s", method, path); // GET /index.html
+
+      char full_path[1024];
+      if (strcmp(path, "/") == 0) {
+          strcpy(full_path, "index.html");
+      } else {
+          snprintf(full_path, sizeof(full_path), "%s", path + 1); // index.html
+      }
+
+      FILE *file = fopen(full_path, "rb");
+
+      // send 404 response if file not found
+      if (file == NULL) {
+        perror("file open failed");
+        char failure_message[] = "<html><body><h1>404 Not Found</h1></body></html>";
+        char buffer[999];
+
+        snprintf(buffer, sizeof(buffer),
+                 "HTTP/1.1 404 Not Found\r\n"
+                 "Content-Length: %lu\r\n"
+                 "Content-Type: text/html\r\n"
+                 "\r\n"
+                 , strlen(failure_message));
+
+        send(client_fd, buffer, strlen(buffer), 0);
+        send(client_fd, failure_message, strlen(failure_message), 0);
+        close(client_fd);
+        continue;
+     }
+
+      fseek(file, 0, SEEK_END);
+      long filesize = ftell(file);
+      rewind(file);
+
+      char *body = malloc(filesize);
+      fread(body, filesize, 1, file);
+      fclose(file);
+
+      char header[999];
+      const char *content_type = get_content_type(full_path);
+
+      snprintf(header, sizeof(header),
+               "HTTP/1.1 200 OK\r\n"
+               "Content-Length: %ld\r\n"
+               "Content-Type: %s\r\n"
+               "\r\n",
+               filesize,
+               content_type);
+
+      printf("this is header: %s", header);
+
+      send(client_fd, header, strlen(header), 0);
+      send(client_fd, body, filesize, 0);
+      free(body);
+      close(client_fd);
+    }
+
+}
+
+
 int main() {
   printf("main function called\n");
   int server_fd;
@@ -49,79 +127,7 @@ int main() {
   printf("Server is running on port 8080\n");
 
   // accept client connection
-  while (1) {
-
-    int client_fd;
-
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-
-    client_fd =
-        accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-
-    // recieve data from client
-    char buffer[7 * 1024 * 1024]; // 7 mb
-    ssize_t recv_response = recv(client_fd, buffer, sizeof(buffer), 0);
-
-    buffer[recv_response] = '\0';
-
-    char method[10], path[999];
-    sscanf(buffer, "%s %s", method, path); // GET /index.html
-
-    char full_path[1024];
-    if (strcmp(path, "/") == 0) {
-        strcpy(full_path, "index.html");
-    } else {
-        snprintf(full_path, sizeof(full_path), "%s", path + 1); // index.html
-    }
-
-    FILE *file = fopen(full_path, "rb");
-
-    // send 404 response if file not found
-    if (file == NULL) {
-      perror("file open failed");
-      char failure_message[] = "<html><body><h1>404 Not Found</h1></body></html>";
-      char buffer[999];
-
-      snprintf(buffer, sizeof(buffer),
-               "HTTP/1.1 404 Not Found\r\n"
-               "Content-Length: %lu\r\n"
-               "Content-Type: text/html\r\n"
-               "\r\n"
-               , strlen(failure_message));
-
-      send(client_fd, buffer, strlen(buffer), 0);
-      send(client_fd, failure_message, strlen(failure_message), 0);
-      close(client_fd);
-      continue;
-   }
-
-    fseek(file, 0, SEEK_END);
-    long filesize = ftell(file);
-    rewind(file);
-
-    char *body = malloc(filesize);
-    fread(body, filesize, 1, file);
-    fclose(file);
-
-    char header[999];
-    const char *content_type = get_content_type(full_path);
-
-    snprintf(header, sizeof(header),
-             "HTTP/1.1 200 OK\r\n"
-             "Content-Length: %ld\r\n"
-             "Content-Type: %s\r\n"
-             "\r\n",
-             filesize,
-             content_type);
-    
-    printf("this is header: %s", header);
-
-    send(client_fd, header, strlen(header), 0);
-    send(client_fd, body, filesize, 0);
-    free(body);
-    close(client_fd);
-  }
+  handle_client(server_fd);
 
   close(server_fd);
 
